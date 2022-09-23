b67626cd33cef027d469dbfb87ccd4aca3cfabcf
SSUCHIYEH <hyoka4654@gmail.com>
2022-09-24 04:11:49 +0800

    [feature] spec調整

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'factories/task'
 require 'faker'
 
 RSpec.describe 'Tasks', type: :request do
+  let!(:task) { create(:task, :init, title: 'TEST') }
   let(:valid_attributes) do
     {
       title: Faker::Hobby.activity,
@@ -16,9 +19,11 @@ RSpec.describe 'Tasks', type: :request do
 
   describe '.get_all' do
     it 'read all tasks' do
-      create(:task, :init)
       get tasks_path
       expect(response).to be_successful
+      expect(response.body).to include('NotStarted')
+      expect(response.body).to include('Medium')
+      expect(response.body).to include('TEST')
     end
   end
 
@@ -31,69 +36,61 @@ RSpec.describe 'Tasks', type: :request do
 
   describe '.edit' do
     it 'edit tasks' do
-      task = create(:task, :init)
       get edit_task_path(task)
       expect(response).to be_successful
     end
   end
 
   describe '.create' do
-    context 'with valid parameters' do
-      it 'creates a new Task' do
-        expect do
-          post tasks_path, params: { task: valid_attributes }
-        end.to change(Task, :count).by(1)
-      end
+    it 'creates a new Task and redirects to the Tasks page' do
+      get new_task_path
+      expect(response).to have_http_status(:ok)
 
-      it 'redirects to tasks' do
-        post tasks_path, params: { task: valid_attributes }
-        expect(response).to redirect_to(tasks_path)
-      end
+      post tasks_path, params: { task: valid_attributes }
+
+      expect(response).to redirect_to(tasks_path)
+      follow_redirect!
+
+      expect(response.body).to include('新增任務成功')
+    end
+
+    it 'does not render a different template' do
+      get new_task_path
+      expect(response).not_to redirect_to(tasks_path)
     end
   end
 
   describe '.update' do
-    context 'with valid parameters' do
-      let(:new_attributes) do
-        {
-          title: Faker::Hobby.activity,
-          content: Faker::Lorem.sentence,
-          start_time: Faker::Date.forward(days: 23),
-          end_time: Faker::Date.forward(days: 23),
-          status: 'NotStarted',
-          priority: 'High'
-        }
-      end
+    let(:new_attributes) do
+      {
+        title: 'UPDATE',
+        content: Faker::Lorem.sentence,
+        start_time: Faker::Date.forward(days: 23),
+        end_time: Faker::Date.forward(days: 23),
+        status: 'NotStarted',
+        priority: 'High'
+      }
+    end
 
-      it 'updates the requested task' do
-        task = create(:task, :important)
-        patch task_path(task), params: { task: new_attributes }
-        last_task = Task.last
-        expect(last_task.title).to eq('task1')
-        expect(last_task.content).to eq('task1')
-      end
+    it 'updates the task and redirects to the Tasks page' do
+      get edit_task_path(task)
+      expect(response).to have_http_status(:ok)
 
-      it 'redirects to the article' do
-        task = create(:task, :important)
-        patch task_path(task), params: { task: new_attributes }
-        task.reload
-        expect(response).to redirect_to(tasks_path)
-      end
+      patch task_path(task), params: { task: new_attributes }
+      task.reload
+      expect(task.title).to eq('UPDATE')
+      expect(task.status).to eq('NotStarted')
     end
   end
 
   describe '.destroy' do
-    it 'destroys the requested task' do
-      task = create(:task, :init)
-      expect do
-        delete task_path(task)
-      end.to change(Task, :count).by(-1)
-    end
-
-    it 'redirects to the tasks list' do
-      task = create(:task, :init)
+    it 'destroy the task and redirects to the tasks page' do
       delete task_path(task)
+
       expect(response).to redirect_to(tasks_path)
+      follow_redirect!
+
+      expect(response.body).to include('任務刪除成功')
     end
   end
 end
