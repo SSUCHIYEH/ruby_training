21b2c200a6fa00a4323466831f83c29cad2f478a
SSUCHIYEH <hyoka4654@gmail.com>
2022-09-26 21:21:09 +0800

    [fix] 程式調整,刪除不必要檔案

--- spec/models/tasks_spec.rb
+++ spec/models/tasks_spec.rb
@@ -13,17 +13,17 @@ RSpec.describe Task, type: :model do
 
     # 錯誤情況
     context "when title is blank" do
-      it {
+      it do
         task.title = ""
         expect { valid }.to change { task.errors.messages[:title].first }.to include("不能為空白")
-      }
+      end
     end
 
     context "when end_time is less_than start_time" do
-      it {
+      it do
         task.end_time = Faker::Date.in_date_period(year: 2022, month: 8)
         expect { valid }.to change { task.errors.messages[:end_time].first }.to include("必須大於或等於")
-      }
+      end
     end
   end
 end
