341cd8ec967aeb8c24f316afdc60acde31b557c6
SSUCHIYEH <hyoka4654@gmail.com>
2022-10-03 23:51:17 +0800

    [feature] bcrypt,路由調整,user新增password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  has_secure_password
 end
---
+++ db/migrate/20221003154257_rename_user_password.rb
@@ -0,0 +1,5 @@
+class RenameUserPassword < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
