4b14e71deacc0a358d4c23032c4e0b2550c726a1
SSUCHIYEH <hyoka4654@gmail.com>
2022-10-06 13:33:18 +0800

    [feature] 新增admin routes, admin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def verify_user!
+    if session[:user_id].present?
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,13 @@ Rails.application.routes.draw do
   # USER
   get 'sign_up', to: 'users#new'
   resources :users, only: [:new, :create]
+  # ADMIN
+  get 'admin', to: 'admin#index'
+  post 'admin', to: 'admin#create', as: 'admin_create_user'
+  delete 'admin', to: 'admin#destroy', as: 'admin_delete_user'
+  patch 'admin', to: 'admin#update', as: 'admin_update_user'
+  get 'admin/new', to: 'admin#new', as: 'admin_new_user'
+  get 'admin/tasks/:id', to: 'admin#user_tasks', as: 'admin_user_tasks'
+  get 'admin/edit/:id', to: 'admin#edit', as: 'admin_edit_user'
   root to: "tasks#index"
 end
